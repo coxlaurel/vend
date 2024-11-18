e020795a8a9c22ea41ee10fde2a4893106f98740
coxlaurel <coxlaurel@gmail.com>
2024-11-18 13:57:15 -0500

    button font

--- main.py
+++ main.py
@@ -4,8 +4,13 @@ from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
 from kivy.uix.screenmanager import Screen, ScreenManager
+from kivy.clock import Clock
+from kivy.metrics import sp
 
 import button as bt
+
+FONT="assets/font/Montserrat-SemiBold.ttf"
+
 
 class MainScreen(GridLayout, Screen):
     """
@@ -13,18 +18,25 @@ class MainScreen(GridLayout, Screen):
 
     Attributes:
         cols: An integer representing the number of columns
+        rows: An integer representing the number of rows
     """
     def __init__(self, screen_manager, **kwargs):
+        """
+        Initialize an instance of Main Screen.
+
+        Args:
+            screen_manager: An instance of screen manager
+        """
         super(MainScreen, self).__init__(**kwargs)
         self.cols = 2
         self.rows = 3
 
-        btn1 = bt.MakeButton("Popcorn", 1, screen_manager=screen_manager)
-        btn2 = bt.MakeButton("Chocolate", 2, screen_manager=screen_manager)
-        btn3 = bt.MakeButton("Candy", 3, screen_manager=screen_manager)
-        btn4 = bt.MakeButton("Twizzler", 4, screen_manager=screen_manager)
-        btn5 = bt.MakeButton("Mike & Ike", 5, screen_manager=screen_manager)
-        btn6 = bt.MakeButton("Tootsie Roll", 6, screen_manager=screen_manager)
+        btn1 = bt.MakeButton("Popcorn", 1, FONT, screen_manager=screen_manager)
+        btn2 = bt.MakeButton("Chocolate", 2, FONT, screen_manager=screen_manager)
+        btn3 = bt.MakeButton("Candy", 3, FONT, screen_manager=screen_manager)
+        btn4 = bt.MakeButton("Twizzler", 4, FONT, screen_manager=screen_manager)
+        btn5 = bt.MakeButton("Mike & Ike", 5, FONT, screen_manager=screen_manager)
+        btn6 = bt.MakeButton("Tootsie Roll", 6, FONT, screen_manager=screen_manager)
 
         self.add_widget(btn1)
         self.add_widget(btn2)
@@ -34,16 +46,43 @@ class MainScreen(GridLayout, Screen):
         self.add_widget(btn6)
 
 
-class VendingScreen(GridLayout, Screen, Label):
+class VendingScreen(GridLayout, Screen):
     """
     Class to create interface when actively vending item.
+
+    Attributes: 
+        cols: An integer representing the number of columns
+        screen_manager: An instance of screen manager
     """
-    def __init__(self, **kwargs):
+    def __init__(self, screen_manager, **kwargs):
+        """
+        Initialize an instance of vending screen.
+
+        Args:
+            screen_manager: An instance of screen manager
+        """
         super(VendingScreen, self).__init__(**kwargs)
         self.cols = 1
+        self.screen_manager = screen_manager
 
-        vend_label = Label(text='Vending...', color=(1, 0, 1, 1))
+        vend_label = Label(text='Vending...',
+                           font_name=FONT,
+                           font_size=sp(60),
+                           color=(1, 0, 1, 1))
         self.add_widget(vend_label)
+
+    def on_enter(self, *args):
+        """
+        This method is called when the screen is displayed.
+        Schedule a return to the main screen after 3 seconds.
+        """
+        Clock.schedule_once(self.go_to_main, 3)
+
+    def go_to_main(self, _):
+        """
+        Method to switch back to the main screen.
+        """
+        self.screen_manager.current = "main"
 
 class VendingApp(App):
     """
@@ -54,7 +93,7 @@ class VendingApp(App):
 
         # Add screens to the ScreenManager
         sm.add_widget(MainScreen(name='main', screen_manager=sm))
-        sm.add_widget(VendingScreen(name='vending'))
+        sm.add_widget(VendingScreen(name='vending', screen_manager=sm))
 
         return sm
 
